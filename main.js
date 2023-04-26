65527437aa620fdb66cac69ea3413c38a0e8bccb
isabelcevallos <icevallo@villanova.edu>
2023-04-26 15:02:04 -0400

    added files

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 
+const board = document.querySelector("#board");
+
+for (let i = 0; i < 42; i++) {
+    let cell = document.createElement("div");
+    cell.className = "cell";
+    board.appendChild(cell);
+}
