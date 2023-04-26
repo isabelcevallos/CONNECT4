7b96f981e82d9a6f8245c5bc7adc8ba2e165fab8
isabelcevallos <icevallo@villanova.edu>
2023-04-26 15:12:34 -0400

    added info

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import main from '/.main';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Connect 4 Game</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+     <main />
   );
 }
 
