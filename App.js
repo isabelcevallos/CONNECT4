40d2be6eed63599e26e722035d481a436053e757
Rene Hart <email.com>
2023-04-27 14:51:59 -0400

    added files

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import home from './home';
-import game from './game';
+import Game from './game'
 
-const Stack = createNativeStackNavigator();
-
- export default function App() {
-   return (
+export default function App() {
+  return (
     <View style={styles.container}>
-      <Text>Connect 4 Game</Text>
+      <Game/>
       <StatusBar style="auto" />
     </View>
-  // <NavigationContainer>
-  //    <Stack.Navigator initial initialRouteName='Home'>
-  //    <Stack.Screen name='Home' component={home} />
-  //    <Stack.Screen name='Game' component={game} />
-  //    </Stack.Navigator>
-  //    </NavigationContainer>
-
-    );
- }
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
