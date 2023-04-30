e8f0e037becffa581b3f0e5d198f1731db2d529d
isabelcevallos <icevallo@villanova.edu>
2023-04-30 00:55:15 -0400

    added homepage and turned it to html

--- game.js
+++ game.js
@@ -1,75 +1,68 @@
-import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity } from 'react-native';
-
-
-
-
-export default function game ({ navigation }) {
-
-    const board = document.querySelector("#board");
-    const modalContainer = document.querySelector("#modal-container");
-    const modalMessage = document.querySelector("#modal-message");
-    const resetButton = document.querySelector("#reset");
+const board = document.querySelector("#board");
+const modalContainer = document.querySelector("#modal-container");
+const modalMessage = document.querySelector("#modal-message");
+const resetButton = document.querySelector("#reset");
    
-    resetButton.onPress = () => {
-      location.reload();
-    }
+resetButton.onclick = () => {
+    location.reload();
+}
    
-    const RED_TURN = 1;
-    const YELLOW_TURN = 2;
+const RED_TURN = 1;
+const YELLOW_TURN = 2;
    
-    // 0 - empty, 1 - red, 2 - yellow
-    const pieces = [
-      0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0,
-      0, 0, 0, 0, 0, 0, 0,
-    ];
+// 0 - empty, 1 - red, 2 - yellow
+const pieces = [
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0,
+];
    
-    let playerTurn = RED_TURN; // 1 - red, 2 - yellow
-    let hoverColumn = -1;
-    let animating = false;
+let playerTurn = RED_TURN; // 1 - red, 2 - yellow
+let hoverColumn = -1;
+let animating = false;
    
-    for (let i = 0; i < 42; i++) {
-      let cell = document.createElement("div");
-      cell.className = "cell";
-      board.appendChild(cell);
+for (let i = 0; i < 42; i++) {
+    let cell = document.createElement("div");
+    cell.className = "cell";
+    board.appendChild(cell);
    
-      cell.onmouseenter = () => {
+    cell.onmouseenter = () => {
         onMouseEnteredColumn(i % 7);
-      }
-      cell.onPress = () => {
-        if(!animating) {
-          onColumnClicked(i % 7);
-        }
-      }
     }
+    cell.onclick = () => {
+        if(!animating) {
+        onColumnClicked(i % 7);
+        }
+    }
+}
    
-    function onColumnClicked(column) {
-      let availableRow = pieces.filter( (_, index) => index % 7 === column).lastIndexOf(0);
-      if(availableRow === -1) {
+function onColumnClicked(column) {
+    let availableRow = pieces.filter( (_, index) => index % 7 === column).lastIndexOf(0);
+    if(availableRow === -1) {
         // no space in the column
         return;
-      }
+    }
    
-      pieces[(availableRow * 7) + column] = playerTurn;
-      let cell = board.children[(availableRow * 7) + column];
+    pieces[(availableRow * 7) + column] = playerTurn;
+    let cell = board.children[(availableRow * 7) + column];
    
-      let piece = document.createElement("div");
-      piece.className = "piece";
-      piece.dataset.placed = true;
-      piece.dataset.player = playerTurn;
-      cell.appendChild(piece);
+    let piece = document.createElement("div");
+    piece.className = "piece";
+    piece.dataset.placed = true;
+    piece.dataset.player = playerTurn;
+    cell.appendChild(piece);
    
-      let unplacedPiece = document.querySelector("[data-placed='false']");
-      let unplacedY = unplacedPiece.getBoundingClientRect().y;
-      let placedY = piece.getBoundingClientRect().y;
-      let yDiff = unplacedY - placedY;
+    let unplacedPiece = document.querySelector("[data-placed='false']");
+    let unplacedY = unplacedPiece.getBoundingClientRect().y;
+    let placedY = piece.getBoundingClientRect().y;
+    let yDiff = unplacedY - placedY;
    
-      animating = true;
-      removeUnplacedPiece();
-      let animation = piece.animate(
+    animating = true;
+    removeUnplacedPiece();
+    let animation = piece.animate(
         [
           { transform: `translateY(${yDiff}px)`, offset: 0},
           { transform: `translateY(0px)`, offset: 0.6},
@@ -81,69 +74,69 @@ export default function game ({ navigation }) {
           easing: "linear",
           iterations: 1
         }
-      );
+    );
       animation.addEventListener('finish', checkGameWinOrDraw)
-    }
+}
    
-    function checkGameWinOrDraw() {
-      animating = false;
+function checkGameWinOrDraw() {
+    animating = false;
    
-      // check if game is a draw
-      if(!pieces.includes(0)) {
+    // check if game is a draw
+    if(!pieces.includes(0)) {
         // game is a draw
         modalContainer.style.display = "block";
         modalMessage.textContent = "Draw";
-      }
+    }
    
-      // check if the current player has won
-      if(hasPlayerWon(playerTurn, pieces)) {
+    // check if the current player has won
+    if(hasPlayerWon(playerTurn, pieces)) {
         // current player has won
         modalContainer.style.display = "block";
         modalMessage.textContent = `${playerTurn === RED_TURN ? "Red" : "Yellow"} WON!`;
         modalMessage.dataset.winner = playerTurn;
-      }
-   
-   
-      if(playerTurn === RED_TURN) {
-        playerTurn = YELLOW_TURN;
-      } else {
-        playerTurn = RED_TURN;
-      }
-   
-      // update hovering piece
-      updateHover();
     }
    
-    function updateHover() {
-      removeUnplacedPiece();
    
-      // add piece
-      if(pieces[hoverColumn] === 0) {
+    if(playerTurn === RED_TURN) {
+    playerTurn = YELLOW_TURN;
+    } else {
+    playerTurn = RED_TURN;
+    }
+   
+    // update hovering piece
+    updateHover();
+}
+   
+function updateHover() {
+    removeUnplacedPiece();
+   
+    // add piece
+    if(pieces[hoverColumn] === 0) {
         let cell = board.children[hoverColumn];
         let piece = document.createElement("div");
         piece.className = "piece";
         piece.dataset.placed = false;
         piece.dataset.player = playerTurn;
         cell.appendChild(piece);
-      }
     }
+}
    
-    function removeUnplacedPiece() {
-      let unplacedPiece = document.querySelector("[data-placed='false']");
-      if(unplacedPiece) {
+function removeUnplacedPiece() {
+    let unplacedPiece = document.querySelector("[data-placed='false']");
+    if(unplacedPiece) {
         unplacedPiece.parentElement.removeChild(unplacedPiece);
-      }
     }
+}
    
-    function onMouseEnteredColumn(column) {
-      hoverColumn = column;
-      if(!animating){
+function onMouseEnteredColumn(column) {
+    hoverColumn = column;
+    if(!animating){
         updateHover();
-      }
     }
+}
    
-    function hasPlayerWon(playerTurn, pieces) {
-      for (let index = 0; index < 42; index++) {
+function hasPlayerWon(playerTurn, pieces) {
+    for (let index = 0; index < 42; index++) {
         // check horiztonal win starting at index
         if(
           index % 7 < 4 &&
@@ -189,8 +182,7 @@ export default function game ({ navigation }) {
         ) {
           return true;
         }
-      }
+    }
    
       return false;
-    }
 }
