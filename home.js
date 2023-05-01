750cdf3cacdeb0449ba3e8b151a025eb80c7a545
isabelcevallos <icevallo@villanova.edu>
2023-05-01 01:55:34 -0400

    deleted the files that we were not using

--- home.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Text, View, FlatList, Button, TouchableOpacity } from 'react-native';
-
-export default function home ({ navigation }) {
-
-return (
-    <Text>Welcome to Connect 4 </Text>
-
-);
-}
